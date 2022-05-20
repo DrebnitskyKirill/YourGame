64df49aafe831c2577199c2d187d3582623e40a5
DrebnitskyKirill <mr.drebnitzky@yandex.ru>
2022-05-20 14:21:58 +0300

    AA

--- client/src/components/Game/Game.jsx
+++ client/src/components/Game/Game.jsx
@@ -1,18 +1,18 @@
-import React, { useState } from 'react'
-import Modal from '../Modal/Modal'
-import './Game.css'
-export default function Game({questions}) {
-  const [state,setState] = useState('notOk')
+import React, { useState } from "react";
+import Modal from "../Modal/Modal";
+import "./Game.css";
+export default function Game({ questions }) {
+  const [state, setState] = useState("notOk");
   const notActive = () => {
-    if(state === 'notOk'){
-      setState('ok')
-    } 
-  }
+    if (state === "notOk") {
+      setState("ok");
+    }
+  };
   return (
     <>
-        <button onClick={notActive} className='theme'><a className={state}>{questions.questionValue}</a></button>
-       </>
-      
-    
-  )
+      <button onClick={notActive} className="theme">
+        <a className={state}>{questions.questionValue}</a>
+      </button>
+    </>
+  );
 }
