034fd7acb83d967dc1db944a3ef8755efc5146fc
DrebnitskyKirill <mr.drebnitzky@yandex.ru>
2022-05-20 10:14:14 +0300

    Seed

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -3,7 +3,7 @@ import "./Modal.css";
 
 export default function Modal({active, setActive}) {
   return (
-    <div className={active ? "modal.active" : "modal"} onClick={()=> setActive(false)}>
+    <div className={active ? "modal.active" : "modal"} onClick={()=> setActive(true)}>
       <div className="modalContent" onClick={e => e.stopPropagation()}>
       Title
       <input placeholder="Ваш ответ"/>
